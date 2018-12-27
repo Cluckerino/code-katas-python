a07e50d948f9cdd4242c4517e07504cddeb59be6
Cluckerino <elitedragonslayer@gmail.com>
2018-12-27 10:22:53 -0800

    Hello world!

---
+++ solutions/hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Hello world module"""
+
+
+def hello_world():
+    """Hello World!"""
+    msg = "Hello World!"
+    print(msg)
+    return msg
+
+
+if __name__ == "__main__":
+    hello_world()
