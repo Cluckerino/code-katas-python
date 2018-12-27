1605f4187bd45a2f7ba5c39bb6ed0e3a09da0e04
Cluckerino <elitedragonslayer@gmail.com>
2018-12-27 13:05:02 -0800

    Stub and tests for kata

---
+++ solutions/fold_an_array.py
@@ -0,0 +1,9 @@
+"""
+Array folding kata.
+https://www.codewars.com/kata/fold-an-array/python
+"""
+
+
+def fold_array(array, runs):
+    """Fold an array for the given number of runs."""
+    return []
