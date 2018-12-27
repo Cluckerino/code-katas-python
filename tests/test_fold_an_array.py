55cac46087fbb41bd8e1e6ac65fd993677aaeeb1
Cluckerino <elitedragonslayer@gmail.com>
2018-12-27 13:17:37 -0800

    Solution for kata

--- tests/test_fold_an_array.py
+++ tests/test_fold_an_array.py
@@ -2,11 +2,22 @@
 
 import unittest
 
-from solutions.fold_an_array import fold_array
+from solutions.fold_an_array import fold_array, fold_array_once
 
 
 class FoldAnArrayTest(unittest.TestCase):
     """Fold-an-array test suite"""
+
+    def test_fold_array_once(self):
+        """Test the single fold fuunction"""
+        # cases = input, expected
+        cases = [
+            ([1, 2, 3, 4, 5], [6, 6, 3]),
+            ([6, 6, 3], [9, 6])
+        ]
+        for inp, exp in cases:
+            with self.subTest(inp=inp):
+                self.assertEqual(fold_array_once(inp), exp)
 
     def test_fold_array_single_run(self):
         """Test fold function for a single fold"""
