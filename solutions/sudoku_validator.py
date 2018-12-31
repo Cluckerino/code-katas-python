332e8a23eb60ef265e321797326c188b3afd107d
Cluckerino <elitedragonslayer@gmail.com>
2018-12-30 17:40:06 -0800

    Stub and tests for kata

---
+++ solutions/sudoku_validator.py
@@ -0,0 +1,9 @@
+"""
+Validate the given sudoku solution.
+https://www.codewars.com/kata/sudoku-solution-validator/python
+"""
+
+
+def validSolution(board):
+    """Check if the given board is a valid sudoku solution. Expects an input as a list of rows."""
+    pass
