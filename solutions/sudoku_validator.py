ee0dc4ef07e5e7d8119c0f2e77bdcff58cc2cf62
Cluckerino <elitedragonslayer@gmail.com>
2018-12-30 18:28:29 -0800

    Added box number getter

--- solutions/sudoku_validator.py
+++ solutions/sudoku_validator.py
@@ -7,3 +7,8 @@ https://www.codewars.com/kata/sudoku-solution-validator/python
 def validSolution(board):
     """Check if the given board is a valid sudoku solution. Expects an input as a list of rows."""
     pass
+
+
+def get_box_num(col_num, row_num):
+    """Get the box number given the column and row."""
+    return 3 * int(row_num/3) + int(col_num/3)
--- tests/test_sudoku_validator.py
+++ tests/test_sudoku_validator.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from solutions.sudoku_validator import validSolution
+from solutions.sudoku_validator import validSolution, get_box_num
 
 
 class SudokuValidatorTest(unittest.TestCase):
@@ -28,6 +28,15 @@ class SudokuValidatorTest(unittest.TestCase):
                              [9, 0, 1, 5, 3, 7, 2, 1, 4],
                              [2, 8, 7, 4, 1, 9, 6, 3, 5],
                              [3, 0, 0, 4, 8, 1, 1, 7, 9]]
+        self.box_nums = [[0, 0, 0, 1, 1, 1, 2, 2, 2],
+                         [0, 0, 0, 1, 1, 1, 2, 2, 2],
+                         [0, 0, 0, 1, 1, 1, 2, 2, 2],
+                         [3, 3, 3, 4, 4, 4, 5, 5, 5],
+                         [3, 3, 3, 4, 4, 4, 5, 5, 5],
+                         [3, 3, 3, 4, 4, 4, 5, 5, 5],
+                         [6, 6, 6, 7, 7, 7, 8, 8, 8],
+                         [6, 6, 6, 7, 7, 7, 8, 8, 8],
+                         [6, 6, 6, 7, 7, 7, 8, 8, 8]]
 
     def test_good_solution(self):
         """Example test from kata"""
@@ -36,3 +45,9 @@ class SudokuValidatorTest(unittest.TestCase):
     def test_bad_solution(self):
         """Example test from kata"""
         self.assertFalse(validSolution(self.bad_solution))
+
+    def test_get_box_num(self):
+        """Test the box number getter"""
+        for r in range(0, 9):
+            for c in range(0, 9):
+                self.assertEqual(get_box_num(c, r), self.box_nums[r][c])
